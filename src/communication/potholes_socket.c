6095a20103950b162495ec0493a4faad89f8306f
silgreen <verdesilvestro@gmail.com>
2022-08-25 17:29:52 +0200

    todo 26/08/2022

--- src/communication/potholes_socket.c
+++ src/communication/potholes_socket.c
@@ -1,10 +1,10 @@
 #include "../lib/com/potholes_socket_lib.h"
 
 /*
-    TODO 25/08/2022
-    2. log completi
-    3. modulo client/accendere il server
-    4. polish codice
+    TODO 26/08/2022
+    1. polish codice con funzioni per assottigliare main
+    2. continuare/ottimizzare logging del server
+    3. aprire aws
 */
 
 int main(int argc, char const *argv[])
